5c9ecc05d70ee0154b0a66d4886215a7f96a3596
Kevin Oliveros <kevin.oliveros@emapta.com>
2025-08-13 20:39:13 +0800

    feat: :sparkles: added pig latin activity and fixed naming for tutorial file

--- 1 - pseudonyms.py
+++ 1 - pseudonyms.py
@@ -1,21 +1,18 @@
-"""
-This module provides a random generator of two names
-"""
+""""Generate funny names by randomly combining names from 2 separate lists."""
+
 import sys
 import random
 
 def main():
-    """
-    Main function to produce names
-    """
+    """Choose names at random from 2 tuples of names and print to screen"""
     print("Welcome to the Psych 'Sidekick Name Picker.'\n")
     print("A name just like Sean would pick for Gus:\n\n")
 
     first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
              "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite'",
              'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
-             'Chewy', 'Chigger', 'Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
-             'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+             'Chewy', 'Chigger', 'Cinnabuns', 'Cleet', 'Cornbread',
+             'Crab Meat', 'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
              'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
              'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
              'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"', 'Mergatroid',
@@ -45,14 +42,17 @@ def main():
     while True:
         first_name = random.choice(first)
         last_name = random.choice(last)
+
         print("\n\n")
         print(f"{first_name} {last_name}", file=sys.stderr)
         print("\n\n")
 
         try_again = input("\n\nTry again? (Press Enter else n to quit)\n")
+
         if try_again.lower() == "n":
             break
 
     input ("\n\nPress Enter to exit")
+
 if __name__ == "__main__":
     main()
---
+++ activities/chapter_1/pig_latin_practice.py
@@ -0,0 +1,23 @@
+print("Let's Create a Pig Latin word shall we?\n")
+
+VOWELS =  ['a', 'e', 'i', 'o', 'u']
+
+
+while True:
+    word = input("Enter a word: ")
+    word_list = list(word)
+
+    if word_list[0] in vowels:
+        word = f"{word}way"
+    else:
+        word_list.pop(0)
+        word = f"{''.join(word_list)}ay"
+    print(word)
+
+    try_again = input("\n\nTry again? (Press Enter else n to quit)\n")
+
+    if try_again.lower() == "n":
+        break
+
+input ("\n\nPress Enter to exit")
+
